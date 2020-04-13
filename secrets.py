daa1be4a35cf28000297cf46ab2590174b9d1820
Isabella Ribeiro <isabellam.ribeiro@gmail.com>
2020-04-13 16:47:49 -0300

    add dummy secrets.py

--- secrets.py
+++ secrets.py
@@ -1,9 +1,9 @@
 #from https://newsapi.org/
-news_org_api_key = 'd8dbc7fe86f64c9a8bb366c5af1fca67'
+news_org_api_key = 'XXXX'
 
 #from https://www.twilio.com/
-twilio_account_sid = 'AC5b91a492da42851c7a83be828eb75cac'
-twilio_auth_token = '766d3d8363abace96567f91f63e3bd82'
+twilio_account_sid = 'XXXX'
+twilio_auth_token = 'XXXX'
 
 #contacts to receive messages {'contact' 'whatsapp:+0000000000000'}
-contact_dict = {'lela': 'whatsapp:+5511981651070'}
+contact_dict = {'lela': 'whatsapp:+XXXX'}
